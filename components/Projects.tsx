5b8a0389caa5fb5a5d1ba47f9e9edc314c5fc40c
Saurabh <saurabhthapliyal20011682@gmail.com>
2024-02-22 15:00:37 +0530

    Changed the images of projects section

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -11,33 +11,35 @@ const roboto = Roboto ({
 
 const projects = [
   {
-    img: 'https://img.freepik.com/free-vector/realistic-phone-text-bubble-collection_23-2149507740.jpg?w=1060&t=st=1705246815~exp=1705247415~hmac=9e00247479393a5820e701752ddffae43e6314a8f13352ecd3419a7b4c73d2b2',
+    img: '/code_fusion.png',
+    name: 'Code Fusion',
+    description: 'A real time collaborative code editor for multiple programming languages',
+    link: 'https://code-fusion-frontend.vercel.app/',
+    tech: 'ReactJS, NodeJS, Javascript, Tailwind-css and Framer-Motion'
+  },
+  {
+    img: '/react-chat.png',
     name: 'Chat App',
     description: "An awesome application that lets you connect with your friends and family anytime, anywhere. 🌐✨",
     link: "https://ww.wxampl.com",
     tech:"React, MongoDB, Nodejs, express and socketIO"
   },
   {
-    img:'https://images.pexels.com/photos/3769747/pexels-photo-3769747.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+    img:'/e-commerce.png',
     name:"E-commerce App",
     description: 'A cutting-edge E-commerce application with a sleek and intuitive user interface.',
     link: 'https://your-ecommerce-app.com',
     tech: 'MongoDb, Express, Node JS, Express and Redux for global state managaement',
   },
+
   {
-    img: 'https://ez-snippet.vercel.app/_next/image?url=%2Fprojects%2Fspotify-clone.gif&w=1920&q=75',
-    name: 'Spotify Clone',
+    img: '/music.png',
+    name: 'Music Streaming Platform',
     description: 'A music streaming app clone with a modern user interface and powerful features.',
     link: 'https://spotify-clone-app.com',
-    tech: 'Next.js 13, React Hook Form, Zustand, Radix UI, Typescript, Tailwind CSS',
+    tech: 'Next.js 13, Typescript, Stripe, Tailwind CSS and Supabase',
   },
-  {
-    img: 'https://ez-snippet.vercel.app/_next/image?url=%2Fprojects%2Fdiscord-clone.jpeg&w=1920&q=75',
-    name: 'Discord Clone',
-    description: 'A messaging and communication app clone with a sleek design and real-time features.',
-    link: 'https://discord-clone-app.com',
-    tech: 'Next.js 13, React Hook Form, Zustand, Shadcn and Typescript'
-  },
+
   // Add more project objects as needed
 ];
 
